e9ad78dc5fc8648e16a0c929bf58e21e189a25ad
Jonathan Stark <jstark@jonathanstark.com>
2013-10-08 12:13:50 -0400

    sorry, i just couldn't deal with 2-space indent

--- php/modernizr-server.php
+++ php/modernizr-server.php
@@ -2,85 +2,85 @@
 
 class Modernizr {
 
-  static $modernizr_js = 'modernizr.js';
-  static $key = 'Modernizr';
+    static $modernizr_js = 'modernizr.js';
+    static $key = 'Modernizr';
 
-  static function boo() {
-    $key = self::$key;
-    if (session_start() && isset($_SESSION) && isset($_SESSION[$key])) {
-      return $_SESSION[$key];
-    } elseif (isset($_COOKIE) && isset($_COOKIE[$key])) {
-      $modernizr = self::_ang($_COOKIE[$key]);
-      if (isset($_SESSION)) {
-        $_SESSION[$key] = $modernizr;
-      }
-      return $modernizr;
-    } else {
-      $f = __DIR__ . DIRECTORY_SEPARATOR . self::$modernizr_js;
-      if (!file_exists($f)) {
-        throw new Exception('Can not find file ' . $f);
-      }
-      print "<html><head><script type='text/javascript'>";
-      readfile($f);
-      print self::_mer() . "</script></head><body></body></html>";
-      exit;
+    static function boo() {
+        $key = self::$key;
+        if (session_start() && isset($_SESSION) && isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        } elseif (isset($_COOKIE) && isset($_COOKIE[$key])) {
+            $modernizr = self::_ang($_COOKIE[$key]);
+            if (isset($_SESSION)) {
+                $_SESSION[$key] = $modernizr;
+            }
+            return $modernizr;
+        } else {
+            $f = __DIR__ . DIRECTORY_SEPARATOR . self::$modernizr_js;
+            if (!file_exists($f)) {
+                throw new Exception('Can not find file ' . $f);
+            }
+            print "<html><head><script type='text/javascript'>";
+            readfile($f);
+            print self::_mer() . "</script></head><body></body></html>";
+            exit;
+        }
     }
-  }
 
-  static function _mer() {
-    return <<<'JAVASCRIPT'
+    static function _mer() {
+        return <<<'JAVASCRIPT'
 
-      var m = Modernizr,
-          c = '';
-      for (var f in m) {
-          if (f[0] == '_') {
-              continue;
-          }
-          var t = typeof m[f];
-          if (t == 'function') {
-              continue;
-          }
-          c += (c ? '|' : 'Modernizr=') + f + ':';
-          if (t == 'object') {
-              for (var s in m[f]) {
-                  c += '/' + s + ':' + (m[f][s] ? '1' : '0');
-              }
-          } else {
-              c += m[f] ? '1' : '0';
-          }
-      }
-      c += ';path=/';
-      try {
-          document.cookie = c;
-          document.location.reload();
-      } catch (e) {}
+            var m = Modernizr,
+                    c = '';
+            for (var f in m) {
+                    if (f[0] == '_') {
+                            continue;
+                    }
+                    var t = typeof m[f];
+                    if (t == 'function') {
+                            continue;
+                    }
+                    c += (c ? '|' : 'Modernizr=') + f + ':';
+                    if (t == 'object') {
+                            for (var s in m[f]) {
+                                    c += '/' + s + ':' + (m[f][s] ? '1' : '0');
+                            }
+                    } else {
+                            c += m[f] ? '1' : '0';
+                    }
+            }
+            c += ';path=/';
+            try {
+                    document.cookie = c;
+                    document.location.reload();
+            } catch (e) {}
 
 JAVASCRIPT;
-  } // Sorry, previous line CAN NOT be indented :(
+    } // Sorry, previous line CAN NOT be indented :(
 
-  static function _ang($cookie) {
-    $modernizr = new Modernizr();
-    foreach (explode('|', $cookie) as $feature) {
-      list($name, $value) = explode(':', $feature, 2);
-      if ($value[0]=='/') {
-        $value_object = new stdClass();
-        foreach (explode('/', substr($value, 1)) as $sub_feature) {
-          list($sub_name, $sub_value) = explode(':', $sub_feature, 2);
-          $value_object->$sub_name = $sub_value;
+    static function _ang($cookie) {
+        $modernizr = new Modernizr();
+        foreach (explode('|', $cookie) as $feature) {
+            list($name, $value) = explode(':', $feature, 2);
+            if ($value[0]=='/') {
+                $value_object = new stdClass();
+                foreach (explode('/', substr($value, 1)) as $sub_feature) {
+                    list($sub_name, $sub_value) = explode(':', $sub_feature, 2);
+                    $value_object->$sub_name = $sub_value;
+                }
+                $modernizr->$name = $value_object;
+            } else {
+                $modernizr->$name = $value;
+            }
         }
-        $modernizr->$name = $value_object;
-      } else {
-        $modernizr->$name = $value;
-      }
+        return $modernizr;
     }
-    return $modernizr;
-  }
 
 }
 
 try {
-  $modernizr = Modernizr::boo();
+    $modernizr = Modernizr::boo();
 } catch(Exception $e) {
-  echo $e->getMessage();
-  die;
+    echo $e->getMessage();
+    die;
 }
