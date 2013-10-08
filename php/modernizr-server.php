4a61955f2e99c613b98a02ff6788e6924d363a14
Jonathan Stark <jstark@jonathanstark.com>
2013-10-08 12:40:35 -0400

    added some js to pull screen info like pixel density and various dimensions

--- php/modernizr-server.php
+++ php/modernizr-server.php
@@ -31,28 +31,33 @@ class Modernizr {
         return <<<'JAVASCRIPT'
 
             var m = Modernizr,
-                    c = '';
+                c = '';
             for (var f in m) {
-                    if (f[0] == '_') {
-                            continue;
+                if (f[0] == '_') {
+                    continue;
+                }
+                var t = typeof m[f];
+                if (t == 'function') {
+                    continue;
+                }
+                c += (c ? '|' : 'Modernizr=') + f + ':';
+                if (t == 'object') {
+                    for (var s in m[f]) {
+                        c += '/' + s + ':' + (m[f][s] ? '1' : '0');
                     }
-                    var t = typeof m[f];
-                    if (t == 'function') {
-                            continue;
-                    }
-                    c += (c ? '|' : 'Modernizr=') + f + ':';
-                    if (t == 'object') {
-                            for (var s in m[f]) {
-                                    c += '/' + s + ':' + (m[f][s] ? '1' : '0');
-                            }
-                    } else {
-                            c += m[f] ? '1' : '0';
-                    }
+                } else {
+                    c += m[f] ? '1' : '0';
+                }
             }
+            c += '|devicepixelratio:' + window.devicePixelRatio;
+            c += '|screenheight:' + screen.height;
+            c += '|screenwidth:' + screen.width;
+            c += '|windowheight:' + window.innerHeight;
+            c += '|windowwidth:' + window.innerWidth;
             c += ';path=/';
             try {
-                    document.cookie = c;
-                    document.location.reload();
+                document.cookie = c;
+                document.location.reload();
             } catch (e) {}
 
 JAVASCRIPT;
